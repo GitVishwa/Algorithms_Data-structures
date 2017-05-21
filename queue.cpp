cfe8fa53a8b9d6a98797ac7153e2b063ac644e4f
Gitvishwa <professionalvishwa@gmal.com>
2017-05-21 21:38:29 +0530

    New Programs

---
+++ queue.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#define MAX_SIZE 9
+
+using namespace std;
+
+class Queue{
+private:
+	int storage[MAX_SIZE];
+	int rear,front;
+public:
+	//constructor
+	Queue();
+	
+	void Enqueue(int );
+	void display();
+	int Dequeue();
+	bool IsEmpty();
+	bool IsFull();
+};
+
+Queue::Queue(){
+
+	front=rear=-1;
+}
+
+bool Queue::IsEmpty() {
+	return (front == -1 && rear == -1);
+}
+
+bool Queue::IsFull() {
+	return (rear+1)%MAX_SIZE == front ? true : false;
+}
+
+void Queue::Enqueue(int item){
+
+	if(IsFull()){
+		cout << "Queue is full " <<endl;	
+		return;
+	}
+	if(IsEmpty()) {
+		front = rear = 0;
+	}
+	else{
+		rear = (rear+1) % MAX_SIZE;
+	}
+	storage[rear] = item;
+}
+
+int Queue::Dequeue(){
+	if(IsEmpty()){
+		cout << "Queue is empty" << endl;
+		return -1;
+	}
+	if(front == rear){
+		rear = front = -1; 
+	}
+	else{
+		front = (front+1)%MAX_SIZE;
+	}
+	return storage[front];
+}
+	
+void Queue::display(){
+	
+	int cnt = (rear+MAX_SIZE-front)%MAX_SIZE + 1;
+		
+	for(int i_index = 0;i_index < cnt;i_index++) {
+		int index = (front+i_index) % MAX_SIZE;
+		cout << storage[index] << endl;
+	}
+}
+int main(void){
+Queue q;
+q.Enqueue(12);
+q.Enqueue(24);
+q.Enqueue(32);
+q.Enqueue(15);
+q.display();
+return 0;
+}
---
+++ stack.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+#define MAX 100
+
+using namespace std;
+
+class Stack{
+
+private:
+	int storage[MAX];
+	int top;
+
+public:
+	//Constructor
+	Stack();
+
+
+	void push(int );
+	int pop();
+	void display();
+	void peek();
+	bool isEmpty();
+
+};
+
+
+Stack::Stack() {
+
+	top = -1;
+}
+
+void Stack::peek() {
+	if(top == -1)
+		throw string("Stack is empty");
+	cout<<"The peek element is "<<storage[top] << endl;
+}
+
+bool Stack::isEmpty() {
+
+	return (top == -1);
+}
+
+void Stack::push(int item){
+	if(top == (MAX - 1)) {
+		cout << "Stack is full Cannot push " << item << endl;
+		return;
+	}
+	else{
+		top++;
+		storage[top] = item;
+	}
+}
+
+
+int Stack::pop() {
+	if(top == -1) {
+		cout << "Stack is empty" << endl;
+		return -1;
+	}
+	int item = storage[top];
+	top--;
+	return item; 
+}
+
+void Stack::display() {
+	int i_index = 0;
+	if(top == -1){
+			cout << "No elements to be displayed " << endl;		
+	}
+	for(i_index = 0;i_index <= top; i_index++){
+		cout << storage[i_index] << " ";
+	}
+	cout << endl;
+}	
+
+int main(void) {
+
+Stack s1;
+s1.push(12);
+s1.push(14);
+s1.push(14);
+s1.peek();
+s1.display();
+s1.pop();
+s1.display();
+s1.pop();
+s1.display();
+s1.pop();
+
+return 0;
+}
